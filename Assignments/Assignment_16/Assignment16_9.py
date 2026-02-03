80e4a3d31c419ee65a17800030d88c3edc8e4a54
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:01:40 +0530

    Update Assignment16_9.py

--- Assignments/Assignment_16/Assignment16_9.py
+++ Assignments/Assignment_16/Assignment16_9.py
@@ -1,2 +1,18 @@
 # 9. Write a program which display first 10 even numbers on screen.
 # Output : 2 4 6 8 10 12 14 16 18 20
+
+def PrintEven():
+    evennumbers = ""
+   
+    for i in range(1, 11):
+        evennumbers += str(i*2) + " " 
+        
+    return evennumbers
+        
+def main():
+    result = PrintEven()
+       
+    print(result)
+
+if __name__ == "__main__":
+    main()
