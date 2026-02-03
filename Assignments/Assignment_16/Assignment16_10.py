bc004aabae18fa2f43b27d9cb425641713299a5c
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:01:55 +0530

    Update Assignment16_10.py

--- Assignments/Assignment_16/Assignment16_10.py
+++ Assignments/Assignment_16/Assignment16_10.py
@@ -1,3 +1,19 @@
 # 10. Write a program which accept name from user and display length of its name.
 # Input : Marvellous 
 # Output : 10
+
+def StringLength(str):
+    count = 0
+    for i in str:
+        count += 1
+    return count
+
+def main():
+    string = input("Enter a name : ")
+       
+    result = StringLength(string)
+       
+    print(result)
+
+if __name__ == "__main__":
+    main()
