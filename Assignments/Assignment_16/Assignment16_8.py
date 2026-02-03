b758488367b237463414954018785a50cfdc7193
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:01:19 +0530

    Update Assignment16_8.py

--- Assignments/Assignment_16/Assignment16_8.py
+++ Assignments/Assignment_16/Assignment16_8.py
@@ -1,3 +1,21 @@
 # 8. Write a program which accept number from user and print that number of “*” on screen.
 # Input : 5 
 # Output : * * * * *
+
+def PrintStar(num):
+    stars = ""
+    while num > 0:
+        stars += "*" + " "
+        num -= 1
+
+    return stars
+
+def main():
+    number = int(input("Enter a number : "))
+       
+    result = PrintStar(number)
+       
+    print(result)
+
+if __name__ == "__main__":
+    main()
