167918cffd6fffa650cdfa00ed59a3d3eea51ce1
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 21:59:58 +0530

    Update Assignment16_4.py

--- Assignments/Assignment_16/Assignment16_4.py
+++ Assignments/Assignment_16/Assignment16_4.py
@@ -4,3 +4,15 @@
 # Marvellous
 # Marvellous
 # Marvellous
+
+def PrintFunc(str):
+    for i in range(5):
+        print(str)
+
+def main():
+    string = input("Enter a number : ")
+       
+    PrintFunc(string)
+
+if __name__ == "__main__":
+    main()
