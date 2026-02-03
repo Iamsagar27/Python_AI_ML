f5185305afe6fa73ccc76154fb72232a286d7e2f
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:12:20 +0530

    Update Assignment21_3.py

--- Assignments/Assignment_21/Assignment21_3.py
+++ Assignments/Assignment_21/Assignment21_3.py
@@ -2,3 +2,32 @@
 # • Use a Lock to avoid race conditions.
 # • Each thread should increment the shared counter multiple times.
 # • Display the final value of the counter after all threads complete execution.
+
+import threading
+
+count = 0
+lobj = threading.Lock()
+
+def Increment():
+    global count
+
+    for _ in range(1000000):
+        with lobj:
+            count += 1
+
+
+def main():
+    global count
+    Thread1 = threading.Thread(target=Increment)
+    Thread2 = threading.Thread(target=Increment)
+
+    Thread1.start()
+    Thread2.start()
+
+    Thread1.join()
+    Thread2.join()
+
+    print("Count :", count)
+
+if __name__ == "__main__":
+    main()
