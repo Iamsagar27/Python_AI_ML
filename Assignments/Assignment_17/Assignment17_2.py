2c5fb525c62042b3a0f7d2b04513fb0740bd497f
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:03:22 +0530

    Update Assignment17_2.py

--- Assignments/Assignment_17/Assignment17_2.py
+++ Assignments/Assignment_17/Assignment17_2.py
@@ -5,3 +5,21 @@
 #           * * * * *
 #           * * * * *
 #           * * * * *
+
+def PrintStars(num):
+    stars = ""
+    for i in range(num):
+        for j in range(num):
+            stars += "*" + " "
+        stars += "\n"
+    return stars
+
+def main():
+    number = int(input("Enter a number : "))
+
+    result= PrintStars(number)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
