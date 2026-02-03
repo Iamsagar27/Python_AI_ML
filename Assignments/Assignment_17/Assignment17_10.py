8f1c853e4555cfe9636dfd83f6f2abbe9db1c39c
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:05:32 +0530

    Update Assignment17_10.py

--- Assignments/Assignment_17/Assignment17_10.py
+++ Assignments/Assignment_17/Assignment17_10.py
@@ -1,3 +1,23 @@
 # 10. Write a program which accept number from user and return addition ofdigits in that number.
 # Input : 5187934 
 # Output : 37
+
+def NumberCount(num):
+    addition = 0
+
+    while num != 0:
+        last = num % 10
+        addition = addition + last 
+        num //= 10
+
+    return addition
+
+def main():
+    number = int(input("Enter a number : "))
+
+    result= NumberCount(number)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
