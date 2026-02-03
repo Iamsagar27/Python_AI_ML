20c2ab1bb8add3c989808b985ad27fb2a6feca90
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:04:13 +0530

    Update Assignment17_5.py

--- Assignments/Assignment_17/Assignment17_5.py
+++ Assignments/Assignment_17/Assignment17_5.py
@@ -1,3 +1,25 @@
 # 5. Write a program which accept one number for user and check whether number is prime or not.
 # Input : 5 
 # Output : It is Prime Number
+
+def PrimeNumber(num):
+    count = 0
+    for i in range(1,num+1):
+        if(num % i == 0):
+            count += 1
+
+    if(count == 2):
+        return "It is Prime Number"
+    else :
+        return "It is not Prime Number"
+
+
+def main():
+    number = int(input("Enter a number : "))
+
+    result= PrimeNumber(number)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
