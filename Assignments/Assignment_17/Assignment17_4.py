3f43f3fd9eb0ccb5edd4279a93e8181ad2073400
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:03:57 +0530

    Update Assignment17_4.py

--- Assignments/Assignment_17/Assignment17_4.py
+++ Assignments/Assignment_17/Assignment17_4.py
@@ -1,3 +1,23 @@
 # 4. Write a program which accept one number form user and return addition of its factors.
 # Input : 12 
 # Output : 16 (1+2+3+4+6)
+
+def FactorialAddition(num):
+    addition = 0
+
+    for i in range(1,num):
+        if(num % i == 0):
+            addition += i
+        
+    return addition
+
+
+def main():
+    number = int(input("Enter a number : "))
+
+    result= FactorialAddition(number)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
