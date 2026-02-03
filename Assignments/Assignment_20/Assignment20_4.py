688b674f5050a96b54a6a8e52e7af7d5361599d8
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:10:14 +0530

    Delete Assignments/Assignment_20 directory

--- Assignments/Assignment_20/Assignment20_4.py
+++
@@ -1,8 +0,0 @@
-# 4. Design a Python application that creates three threads named Small, Capital, and Digits.
-# • All threads should accept a string as input.
-# • The Small thread should count and display the number of lowercase characters.
-# • The Capital thread should count and display the number of uppercase characters.
-# • The Digits thread should count and display the number of numeric digits.
-# • Each thread must also display:
-# ◦ Thread ID
-# ◦ Thread Name
