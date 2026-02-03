274c59663bedc206c015af14e913b18e2ffcb8d9
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:03:40 +0530

    Update Assignment17_3.py

--- Assignments/Assignment_17/Assignment17_3.py
+++ Assignments/Assignment_17/Assignment17_3.py
@@ -1,3 +1,24 @@
 # 3. Write a program which accept one number from user and return its factorial.
 # Input : 5 
 # Output : 120
+
+def Factorial(num):
+    fact = 1
+    if(num == 0):
+        return fact
+    
+    for i in range(1,num+1):
+        fact *= i
+
+    return fact
+
+
+def main():
+    number = int(input("Enter a number : "))
+
+    result= Factorial(number)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
