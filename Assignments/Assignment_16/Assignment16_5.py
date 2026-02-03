abf710bb031b24f7b1b00fbe3994c057bbe9c83c
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:00:20 +0530

    Update Assignment16_5.py

--- Assignments/Assignment_16/Assignment16_5.py
+++ Assignments/Assignment_16/Assignment16_5.py
@@ -1,2 +1,20 @@
 # 5. 5.Write a program which display 10 to 1 on screen.
 # Output : 10 9 8 7 6 5 4 3 2 1
+
+def DisplayNumbers(num):
+    numbers = ""
+
+    for i in range(num,0,-1):
+        numbers += str(i) + " "
+
+    return numbers
+
+def main():
+    number = int(input("Enter a number : "))
+       
+    result = DisplayNumbers(number)
+       
+    print(result)
+
+if __name__ == "__main__":
+    main()
