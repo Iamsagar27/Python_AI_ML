8ae86a283a62e4e1131929c36cbf67608c2cb1af
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:05:17 +0530

    Update Assignment17_9.py

--- Assignments/Assignment_17/Assignment17_9.py
+++ Assignments/Assignment_17/Assignment17_9.py
@@ -1,3 +1,22 @@
 # 9. Write a program which accept number from user and return number of digits in that number.
 # Input : 5187934 
 # Output : 7
+
+def NumberCount(num):
+    count = 0
+
+    while num != 0:
+        num //= 10
+        count += 1
+
+    return count
+
+def main():
+    number = int(input("Enter a number : "))
+
+    result= NumberCount(number)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
