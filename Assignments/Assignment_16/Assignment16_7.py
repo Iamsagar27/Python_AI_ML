e3c962f93a364c04b3333900ca6fa6c5cb6290fc
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:01:03 +0530

    Update Assignment16_7.py

--- Assignments/Assignment_16/Assignment16_7.py
+++ Assignments/Assignment_16/Assignment16_7.py
@@ -1,3 +1,20 @@
-# 7. Write a program which contains one function that accept one number from user and returns true if number is divisible by 5 otherwise return false.
+# 7. Write a program which contains one function that accept one number from user and returns true if number is 
+# divisible by 5 otherwise return false.
 # Input : 8 Output : False
 # Input : 25 Output : True
+
+def CheckNumber(num):
+    if(num % 5 == 0):
+         return True
+    else :
+         return False
+
+def main():
+    number = int(input("Enter a number : "))
+       
+    result = CheckNumber(number)
+       
+    print(result)
+
+if __name__ == "__main__":
+    main()
