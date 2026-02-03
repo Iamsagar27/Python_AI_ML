9c0197e9c996e5922651d6986971e519615641e9
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:05:00 +0530

    Update Assignment17_8.py

--- Assignments/Assignment_17/Assignment17_8.py
+++ Assignments/Assignment_17/Assignment17_8.py
@@ -5,3 +5,22 @@
 #           1 2 3
 #           1 2 3 4
 #           1 2 3 4 5
+
+def PrintNumbers(num):
+    numbers = ""
+    for i in range(1,num+1):
+        for j in range(1,i+1):
+            numbers += str(j) + " "
+        numbers += "\n"
+        num -= 1
+    return numbers
+
+def main():
+    number = int(input("Enter a number : "))
+
+    result= PrintNumbers(number)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
