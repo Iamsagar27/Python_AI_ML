f129b4bcd6945e5728e8cf850d57299519c3cebf
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-03 22:07:59 +0530

    Delete Assignments/Assignment_18 directory

--- Assignments/Assignment_18/Assignment18_4.py
+++
@@ -1,6 +0,0 @@
-# 4. Write a program which accept N numbers from user and store it into List.
-# Accept one another number from user and return frequency of that number from List.
-# Input : Number of elements : 11
-# Input Elements : 13 5 45 7 4 56 5 34 2 5 65
-# Element to search : 5
-# Output : 3
