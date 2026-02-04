a1887f33c9edf633d6a2063f840496851809d957
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2026-02-04 13:27:48 +0530

    Update Assignment22_2.py

--- Assignments/Assignment_22/Assignment22_2.py
+++ Assignments/Assignment_22/Assignment22_2.py
@@ -1,3 +1,14 @@
+# 2: Write a Python program to implement a class named Circle with the following requirements:
+# The class should contain three instance variables: Radius, Area. and Circumference.
+# The class should contain one class variable named PI, initialized to 3.14.
+# Define a constructor (init) that initializes all instance variables to 0.0.
+# Implement the following instance methods:
+# Accept()accepts the radius of the circle from the user.
+# CalculateArea() calculates the area of the circle and stores it in the Area variable.
+# CalculateCircumference() calculates the circumference of the circle and stores it in the Circumference variable.
+# Display()-displays the values of Radius, Area, and Circumference.
+# Create multiple objects of the Circle class and invoke all the instance methods for each object.
+
 class Circle:
     PI = 3.14
 
@@ -26,3 +37,4 @@ Obj.Accept()
 Obj.CalculateArea()
 Obj.CalculateCircumference()
 Obj.Display()
+
